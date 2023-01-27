ee997d13cfc818ecbefaf31e999ba0528d1f1c1d
sarahmarkland <sarah.markland@icloud.com>
2023-01-27 09:37:34 -0800

    task 0 commit 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+
+{
+int _putchar(char c);
+}
+
+#endif
