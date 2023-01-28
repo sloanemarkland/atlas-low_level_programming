85e8ecad6007b3e187a90cb08e75fc727b944618
sarahmarkland <sarah.markland@icloud.com>
2023-01-28 15:54:18 -0800

    task 6 commit 1

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _abs - absolute value of int
+* @n: if pos num, print, else * -1
+* Return: Always 0
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+	return (n);
+	}
+	else
+	{
+	return (n * -1);
+	}
+}
