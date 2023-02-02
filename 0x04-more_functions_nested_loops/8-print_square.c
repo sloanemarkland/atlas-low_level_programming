2847b03ee168cf95bdb108056f47962a6d934708
sarahmarkland <sarah.markland@icloud.com>
2023-02-02 12:47:42 -0800

    task 8 commit 1

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_square - print a square
+*
+* Return: Always 0
+*/
+
+void print_square(int size)
+{
+	int row;
+	int column;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+		for (row = 0 ; row < size ; row++)
+		{
+			for (column = 0 ; column < size ; column++)
+			{
+				_putchar('#');
+			}
+		_putchar('#');
+		_putchar('\n');
+		}
+}
