88669354d292fba89383d1a91e7dd7e2b4d8d913
sarahmarkland <sarah.markland@icloud.com>
2023-02-08 12:00:33 -0800

    task 0 commit 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+* _strcat - concatenate dest+src+\0
+* @dest: destination
+* @src: source
+* Return: Always 0
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		;
+	}
+	for (j = 0; src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+	return (dest);
+}
