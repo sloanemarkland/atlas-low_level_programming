2821ab6b6c31103ffb8451415f0ae62841bcd578
sarahmarkland <sarah.markland@icloud.com>
2023-02-09 18:52:59 -0800

    task 1 commit without curly braces and both var = 0

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,12 +11,12 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j;
+	int j = 0;
 
 	while (dest[i] != '\0')
 		i++;
 
-	for (j = 0; j < n && src[j] != '\0'; j++)
+	for (; j < n && src[j] != '\0'; j++)
 		dest[i + j] = src[j];
 
 	dest[i + j] = '\0';
