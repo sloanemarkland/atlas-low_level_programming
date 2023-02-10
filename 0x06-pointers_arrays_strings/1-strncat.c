45b479d069328b7637a695401396734d8c20151e
sarahmarkland <sarah.markland@icloud.com>
2023-02-09 18:47:19 -0800

    task 1 without curly brax

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,14 +14,11 @@ char *_strncat(char *dest, char *src, int n)
 	int j;
 
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
-	{
 		dest[i + j] = src[j];
-	}
+
 	dest[i + j] = '\0';
 	return (dest);
 }
