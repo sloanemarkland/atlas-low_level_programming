c3796c6ada331612197d8d391a14582ddc14e407
sarahmarkland <sarah.markland@icloud.com>
2023-02-10 13:59:57 -0800

    task 5 commit 1

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* string_toupper - change lowercase letters to uppercase
+* @str: string variable
+* Return: Always 0
+*/
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	for (; str[i]; i++)
+
+	if (str[i] >= 97 && str[i] <= 122)
+		str[i] = str[i] - 32;
+
+	return (str);
+}
