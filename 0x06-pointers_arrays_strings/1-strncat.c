ec204a3a7195738aa8cd46ffbf9db03faa2612ee
sarahmarkland <sarah.markland@icloud.com>
2023-02-09 18:55:39 -0800

    task 1 commit like stamper told me

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,12 +13,12 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	while (dest[i] != '\0')
+	while (dest[i])
 		i++;
 
-	for (; j < n && src[j] != '\0'; j++)
-		dest[i + j] = src[j];
+	for (; j < n && src[j]; i++, j++)
+		dest[i] = src[j];
 
-	dest[i + j] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
