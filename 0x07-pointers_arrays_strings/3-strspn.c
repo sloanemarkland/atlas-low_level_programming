8e0233642d079fe098d90005db842155a9622689
sarahmarkland <sarah.markland@icloud.com>
2023-02-12 16:11:34 -0600

    task 3 infinite loop

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,1 +1,24 @@
 #include "main.h"
+/**
+* _strspn - get length of prefix substring
+* @s: asdf
+* @accept: prefix substring
+* Return: Always 0
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, count = 0;
+
+	for (i = 0; accept[i]; i++)
+	{
+		j = 0;
+		while ((s[j] >= 'a' && s[j] <= 'z') || (s[j] >= 'A' && s[j] <= 'Z'))
+		{
+			if (accept[i] == s[j])
+			count++;
+		}
+		j++;
+	}
+	return (count);
+}
