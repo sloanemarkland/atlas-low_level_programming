bb4cafe252e045047aff998fa647787f7455aa16
sarahmarkland <sarah.markland@icloud.com>
2023-02-11 19:43:18 -0800

    task 7 commit 1

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* leet - replace AEOTLaeotl with 43071
+* @str: string of Real speak
+* Return: Always 0
+*/
+
+char *leet(char *str)
+{
+	int i = 0;
+	int j;
+	char real[] = "AaEeOoTtLl";
+	char leet[] = "4433007711";
+
+	while (str[i])
+	{
+		for (j = 0; real[j]; j++)
+		{
+			if (str[i] == real[j])
+			{
+				str[i] = leet[j];
+			}
+		}
+	}
+	return (str);
+}
+
