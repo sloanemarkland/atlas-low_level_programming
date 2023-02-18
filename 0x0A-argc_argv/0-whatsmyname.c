4e5fe0f3dbc46959c6fe7205fa89a89953694d14
sarahmarkland <sarah.markland@icloud.com>
2023-02-18 12:44:54 -0800

    task 0 commit 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - oo na na, print my name; write func that prints its own name
+* @argc: num of command-line args passed to program
+* @argv: array of pointers to command-line args
+* $mrklnd
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	if (argc)
+	{}
+		printf("%s\n", argv[0]);
+		return (0);
+
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+void printFunctionName();
+
+#endif
