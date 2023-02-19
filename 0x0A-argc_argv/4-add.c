c3ed29c84040d7595a8134d3cc00e60e43190554
sarahmarkland <sarah.markland@icloud.com>
2023-02-19 14:46:29 -0800

    task 4 commit 1

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
-* main - add positive int; if no num, print 0;
-* if num contains symbol, print Error & return 1
+* main - add positive int. if no num, print 0.
 * @argc: num of command-line args passed to program
 * @argv: array of pointers to command-line args
 * $mrklnd
@@ -10,3 +9,28 @@
 */
 
 int main(int argc, char *argv[])
+{
+	int i, j, sum; /**i refers to which string, j is element in str*/
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
