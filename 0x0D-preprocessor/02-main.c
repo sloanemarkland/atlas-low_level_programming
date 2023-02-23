1b939222707e2fec86755f863fae7719b52a65b8
sarahmarkland <sarah.markland@icloud.com>
2023-02-23 12:15:41 -0800

    task 2 commit 3

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "stdlib.h"
 /**
  * main - prints the name of the file it was compiled from
  *
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
