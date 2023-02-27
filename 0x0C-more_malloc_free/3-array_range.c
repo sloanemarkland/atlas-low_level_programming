8247653a94dee515f155b7c67925cc176ecb4e28
sarahmarkland <sarah.markland@icloud.com>
2023-02-27 09:23:02 -0800

    task 3 commit 1

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* array_range - create an array of ints
+* @min: lowest number
+* @max: highest number
+* $mrklnd
+* Return: ptr to array
+*/
+int *array_range(int min, int max)
+{
+	int i;
+	int *arr;
+
+	arr = (int *) malloc((max - min + 1) * sizeof(int));
+
+	if (min > max || arr == NULL)
+		return (NULL);
+
+	for (i = 0; i <= max - min; i++)
+	{
+		arr[i] = min + i;
+	}
+	return (arr);
+}
