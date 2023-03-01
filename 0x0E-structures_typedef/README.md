b6aa631452a03ed61d928051baf6a428f0e69c56
sarahmarkland <sarah.markland@icloud.com>
2023-03-01 10:24:41 -0800

    task 0 commit 1

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Readme for Structs and stuff
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - new puppy details
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
