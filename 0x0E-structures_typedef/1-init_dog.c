6528bd5df0139000aff552e810776c6a9a26165b
sarahmarkland <sarah.markland@icloud.com>
2023-03-01 12:30:09 -0800

    task 1 commit 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+* init_dog - initialize a variable of type struct dog
+* @d: instance of struct dog
+* @name: pointer being passed in
+* @age: of dog in instance
+* @owner: pointer to owner name in struct
+* Return: Always 0
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 /**
  * struct dog - new puppy details
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
  *
- * Description: Longer description
+ * Description: That's about all there is to it.
  */
 struct dog
 {
@@ -15,5 +20,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
