41872bfb0ed02bc86796e1495051b709a759b039
sarahmarkland <sarah.markland@icloud.com>
2023-03-02 10:50:47 -0800

    task 4 commit 2?

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,20 +2,35 @@
 
 /**
 * new_dog - create a new dog
-* @name: 
-* @age:
-* @owner: 
+* @name: store copy
+* @age: return
+* @owner: store copy
 * Return: Always 0
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *puppy;
 
-	puppy = malloc(sizeof(dog_t));
+	puppy = (dog_t *)malloc(sizeof(dog_t));
 	if (puppy == NULL)
 		return (NULL);
-	puppy->name = name;
+/*allocate mem for name and copy string*/
+	puppy->name = (char *)malloc(strlen(name) + 1);
+	if (puppy->name == NULL)
+	{
+		free(puppy);
+		return (NULL);
+	}
+	strcpy(puppy->name, name);
+
+	puppy->owner = (char *)malloc(strlen(owner) + 1);
+	if (puppy->owner == NULL)
+	{
+		free(puppy->name);
+		free(puppy);
+		return (NULL);
+	}
 	puppy->age = age;
-	puppy->owner = owner;
+
 	return (puppy);
 }
