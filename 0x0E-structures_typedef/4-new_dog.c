32ff821b22f7d67c9709aea63543d9c8b65a9cae
sarahmarkland <sarah.markland@icloud.com>
2023-03-02 11:25:34 -0800

    task 4 commit to fix mixed declarations

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,26 +10,44 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *puppy;
+	int i;
+	int name_length;
+	int owner_length;
 
-	puppy = (dog_t *)malloc(sizeof(dog_t));
+	puppy = (dog_t *) malloc(sizeof(dog_t));
 	if (puppy == NULL)
 		return (NULL);
 /*allocate mem for name and copy string*/
-	puppy->name = (char *)malloc(strlen(name) + 1);
+	name_length = 0;
+
+	while (name[name_length])
+		name_length++;
+	puppy->name = (char *) malloc(name_length + 1);
 	if (puppy->name == NULL)
 	{
 		free(puppy);
 		return (NULL);
 	}
-	strcpy(puppy->name, name);
+	for (i = 0; i <= name_length; i++)
+		puppy->name[i] = name[i];
+/*allocate mem for owner and copy string*/
+	owner_length = 0;
 
-	puppy->owner = (char *)malloc(strlen(owner) + 1);
+	while (owner[owner_length])
+		owner_length++;
+
+	puppy->owner = (char *) malloc(owner_length + 1);
+
 	if (puppy->owner == NULL)
 	{
 		free(puppy->name);
 		free(puppy);
 		return (NULL);
 	}
+
+	for (i = 0; i <= owner_length; i++)
+		puppy->owner[i] = owner[i];
+
 	puppy->age = age;
 
 	return (puppy);
