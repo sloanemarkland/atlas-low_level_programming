75790351108f442f841b5154d18c72bbedf403b2
sarahmarkland <sarah.markland@icloud.com>
2023-03-03 15:03:02 -0800

    task 1 commit 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - func that executes a func given as parameter
+ * on ea element of an array
+ * @size: size of array
+ * @action: ptr to func you need to use
+ * @array: given array
+ * Return: Nothing
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
