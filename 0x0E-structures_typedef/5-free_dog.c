f4d1d9fd2e8feb435eb9c1dec1271ce2c26462ca
sarahmarkland <sarah.markland@icloud.com>
2023-03-03 08:59:41 -0800

    task 5 commit 1

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,11 +1,14 @@
 #include "dog.h"
 
 /**
-* print_dog - print a struct dog instance
+* free_dog - func that frees dog
 * @d: instance of struct dog
 * Return: Always 0
 */
 void free_dog(dog_t *d)
 {
-
+	free(d->name);
+	free(d->owner);
+	free(d);
+	return;
 }
