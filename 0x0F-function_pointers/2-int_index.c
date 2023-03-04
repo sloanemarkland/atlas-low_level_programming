a9879369e1821dd7973a6f4289ec46e16171aa3b
sarahmarkland <sarah.markland@icloud.com>
2023-03-04 11:11:21 -0800

    task 2 commit 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,8 +11,8 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-
-	if (size <= 0)
+/*why is it not if size <= 0 return -1?*/
+	if (array == NULL || cmp == NULL)
 		return (-1);
 
 /*
