c91394417b6975497521a83ae5848c56e42896bd
sarahmarkland <sarah.markland@icloud.com>
2023-03-04 11:05:34 -0800

    task 2 commit 1

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an int, return index of 1st lmnt
+ * @size: size of array
+ * @cmp: ptr to func used to compare values
+ * @array: given array
+ * Return: index of first element for which cmp does not
+ * return 0. If no match, return -1
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+/*
+* loop thru array lmnts and call cmp for each
+* if cmp returns non-zero, return index of element
+*/
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
