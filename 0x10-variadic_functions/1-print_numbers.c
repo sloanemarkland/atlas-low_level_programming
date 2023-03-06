255671877c7052384795c52b43ce49d5b7938df0
sarahmarkland <sarah.markland@icloud.com>
2023-03-06 13:13:00 -0800

    task 1 commit 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,11 +1,36 @@
-#include “variadic_functions.h”
-
+#include "variadic_functions.h"
+#include "stdarg.h"
 /**
-* func - desc
-* @ : desc $
-* Return: 
+* print_numbers - print numbers followed by a new line
+* @separator: string to be printed between number$
+* @n: number of ints passed to the function
+* Return: 0
 */
-
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	int num;
+	unsigned int i;
+	va_list print_args;
 
+	va_start(print_args, n);
+
+	if (separator == NULL)
+	{
+		return;
+	}
+	/*loop over args in list and print them*/
+	for (i = 0; i < n; i++)
+	{
+		/*retrieve next arg from list with va_arg*/
+		num = va_arg(print_args, int);
+		/*print number and separator (if not the last number)*/
+		printf("%d", num);
+
+		if (i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(print_args);
+	printf("\n");
 }
