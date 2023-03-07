9803e6cb8668f7bc3461723f8cac9244cb237a93
sarahmarkland <sarah.markland@icloud.com>
2023-03-06 18:36:50 -0800

    task 3 commit for the heck of it

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,11 +1,44 @@
-#include “variadic_functions.h”
-
+#include "variadic_functions.h"
+#include <stdarg.h>
 /**
-* func - desc
-* @ : desc $
-* Return: 
+* print_all - prints ANYTHING
+* @format: list of types of argument$
+* Return: nada
 */
-
+void print_all(const char * const format, ...)
 {
+	va_list schmorg_args;
+	const char *p = format;
+	int i = 0;
+	float f;
+	char *s;
 
+	va_start(schmorg_args, format);
+
+	while (*p)
+	{
+		if (*p == 'c')
+		{
+			i = va_arg(schmorg_args, int);
+			printf("%c", i);
+		}
+		else if (*p == 'd')
+		{
+			i = va_arg(schmorg_args, int);
+			printf("%d", i);
+		}
+		else if (*p == 'f')
+		{
+			f = va_arg(schmorg_args, double);
+			printf("%f", f);
+		}
+		else if (*p == 's')
+		{
+			s = va_arg(schmorg_args, char *);
+			printf("%s", s);
+		}
+		p++;
+	}
+	printf("\n");
+	va_end(schmorg_args);
 }
