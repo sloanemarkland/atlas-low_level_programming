23e135856907e0c4fb2ee4f7760b367631e2a5ff
sarahmarkland <sarah.markland@icloud.com>
2023-03-06 17:03:45 -0800

    task 2 commit 1

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,11 +1,35 @@
-#include “variadic_functions.h”
+#include "variadic_functions.h"
+#include "stdarg.h"
 
 /**
-* func - desc
-* @ : desc $
-* Return: 
+* print_strings - print strings followed by new line
+* @separator: string to be printed btwn string$
+* @n: number of strings passed to the function
+* Return: 0
 */
-
+void print_strings(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+	va_list string_args;
 
+	va_start(string_args, n);
+
+	if (separator == NULL)
+	{
+		printf("nil\n");
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		char *str_chz = va_arg(string_args, char*);
+
+		printf("%s", str_chz);
+
+		if (i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(string_args);
 }
