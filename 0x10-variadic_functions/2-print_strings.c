4cf6b9bf9f6d4c33b88336cd81c03d9ca17488a3
sarahmarkland <sarah.markland@icloud.com>
2023-03-06 17:08:39 -0800

    task 2 commit 2 with NULL again ugh

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		printf("%s", str_chz);
 
-		if (i < (n - 1))
+		if (i < (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
