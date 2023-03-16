f49d38be9ea74de45da6a6f9c307bb3ff5c6bd91
sarahmarkland <sarah.markland@icloud.com>
2023-03-16 10:42:19 -0500

    task 0 commit 1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * @h: ptr to string to print
+ * Return: number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *current = h;
+	
+	while (current)
+	{
+		if (!(current->str))
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", current->len, current->str);
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
