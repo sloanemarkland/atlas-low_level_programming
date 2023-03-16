b096c0422ae0299c8d9832c7001377b01ca79880
sarahmarkland <sarah.markland@icloud.com>
2023-03-16 14:27:10 -0500

    task 2 added strlen

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,23 +9,19 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	static int count;
 
 	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
-		return (NULL);
-
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
-	count = 0;
 
-	count++;
 	return (new_node);
 }
