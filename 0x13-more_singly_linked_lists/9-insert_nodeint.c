6ac1e8246662fb81d95090d03862b66fffdb45b6
sarahmarkland <sarah.markland@icloud.com>
2023-03-18 20:22:24 -0500

    task 9 fix silly error that made things worse

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	new_node = malloc(sizeof(listint_t));
-	
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
