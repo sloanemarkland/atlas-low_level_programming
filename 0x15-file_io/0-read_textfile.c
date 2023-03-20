170909acf66d02f8d53c0e4a6716dbae998d6cb9
sarahmarkland <sarah.markland@icloud.com>
2023-03-20 16:26:52 -0500

    task 0 commit 1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file
+ * @letters: number of letters it should read and print
+ * Return: actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	/**
+	 * this func takes a string 'filename' and an int 'letters'
+	 * it opens the file, reads it, and prints it to stdout
+	*/
+	
+	int fd, rd, wr;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	rd = read(fd, buf, letters);
+	if (rd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	wr = write(STDOUT_FILENO, buf, rd);
+	if (wr == -1 || wr != rd)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (wr);
+}
