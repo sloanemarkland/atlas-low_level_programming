6f0de6dde8f13c60f2fdc716c89743b5e01e9502
sarahmarkland <sarah.markland@icloud.com>
2023-03-20 17:28:04 -0500

    task 2 commit 1

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end of the file
+ * Return: 1 on success, -1 on failure
+ */
+	int append_text_to_file(const char *filename, char *text_content)
+	{
+		int fd, wr, len;
+
+		if (filename == NULL)
+			return (-1);
+
+		fd = open(filename, O_WRONLY | O_APPEND);
+		if (fd == -1)
+			return (-1);
+
+		if (text_content == NULL)
+		{
+			close(fd);
+			return (1);
+		}
+
+		for (len = 0; text_content[len]; len++)
+			;
+
+		wr = write(fd, text_content, len);
+		if (wr == -1)
+			return (-1);
+
+		close(fd);
+		return (1);
+	}
