2f783ef3bca5b0cf573878a2bf15be7c706cb26c
sarahmarkland <sarah.markland@icloud.com>
2023-03-22 11:23:02 -0500

    doubly linked lists

---
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_dlistint - print elements of a list
+ * @h: ptr to data
+ * 
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+	const dlistint_t *current = h;
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		i++;
+	}
+
+	return (i);
+}
---
+++ 0x18-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Readme for 0x18 doubly linked lists
