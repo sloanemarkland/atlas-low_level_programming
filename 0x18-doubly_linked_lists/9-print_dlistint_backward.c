fe4ca89a2147e4de07f95f8a18b887ea7970b4ff
sarahmarkland <sarah.markland@icloud.com>
2023-03-23 11:27:04 -0500

    task 9 try cha cha code

---
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_dlistint_backward - does what it says
+ * @h: pointer to the head of the list
+ *
+ * $m
+ * Return: number of nodes
+ */
+size_t print_dlistint_backward(const dlistint_t *h)
+{
+	size_t count = 0;
+	const dlistint_t *current = h;
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->prev;
+		count++;
+	}
+
+	return (count);
+}
