548aa11856d2f9b43a906cba4854492b0e832de2
sarahmarkland <sarah.markland@icloud.com>
2023-03-27 12:44:47 -0500

    task 3 commit betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	current = ht->array [index];
+	current = ht->array[index];
 
 	while (current)
 	{
