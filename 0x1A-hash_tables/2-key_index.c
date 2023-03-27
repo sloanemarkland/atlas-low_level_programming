ee7dbf9f4fc25dba2b8be81e0ad393d4c6466bce
sarahmarkland <sarah.markland@icloud.com>
2023-03-27 12:43:56 -0500

    task 3 commit 1

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,7 +7,10 @@
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+	/*call hash func passing in 'key' ptr, should return hash code for key*/
 	unsigned long int hash_code = hash_djb2(key);
+	/*calc index to store key/value pair*/
+	/*use modulo on the hash code to get val between 0 & 'size - 1'*/
 	unsigned long int index = hash_code % size;
 
 	return (index);
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,44 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - add an element to a hash table
+ * @ht: hash table to update key/val pair to
+ * @key: key to be indexed
+ * @value: must be duped, can be empty string
+ * Return: 1 if succeeded, 0 otherwise
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *current, *entry = NULL;
+	char *newVal = NULL;
+
+	if (!ht || !key || !value)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array [index];
+
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			newVal = strdup(value);
+			free(current->value);
+			current->value = newVal;
+			return (1);
+		}
+		current = current->next;
+	}
+	entry = malloc(sizeof(hash_node_t));
+	if (entry == NULL)
+	{
+		free(entry);
+		return (0);
+	}
+	entry->key = strdup(key);
+	entry->value = strdup(value);
+	entry->next = ht->array[index];
+	ht->array[index] = entry;
+	return (1);
+}
