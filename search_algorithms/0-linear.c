3532db44610cd856ec3da2f714b53be8800f1558
sarahmarkland <sarah.markland@icloud.com>
2023-12-21 13:33:29 -0600

    task 0 i love C

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -9,8 +9,19 @@
 * $rhmrklnd
 * Return: 0
 */
-int linear_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value)
 {
-	
+	size_t i = 0;
 
+	if (array)
+	{
+		while (i < size)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+				return (i);
+			i++;
+		}
+	}
+	return (-1);
 }
