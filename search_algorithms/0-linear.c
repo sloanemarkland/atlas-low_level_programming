f2f6d71d34618a0e40e2392216afae07b484e041
sarahmarkland <sarah.markland@icloud.com>
2023-12-21 11:25:17 -0600

    I MISSED C SO MUCH

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,16 @@
+#include "search_algos.h"
+
+/**
+* linear_search - function that searches for a value in an array of
+* integers using the Linear search algorithm
+* @array: num of command-line args passed to program
+* @size: array of pointers to command-line args
+* @value: array of pointers to command-line args
+* $rhmrklnd
+* Return: 0
+*/
+int linear_search(int *array, size_t size, int value);
+{
+	
+
+}
---
+++ search_algorithms/README.md
@@ -0,0 +1,1 @@
+Readme for Search Algs
